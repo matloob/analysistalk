c44d5eb894620142f81b2ad215b9c961ba06cfa7
Michael Matloob <matloob@golang.org>
2019-08-16 15:08:39 -0400

    cmpequal: add a suggested fix

--- cmpequal/cmpequal.go
+++ cmpequal/cmpequal.go
@@ -1,20 +1,22 @@
 package cmpequal
 
 import (
+	"bytes"
+	"fmt"
 	"go/ast"
 	"go/types"
+
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 	"golang.org/x/tools/go/types/typeutil"
 )
 
-var Analyzer = &analysis.Analyzer {
-	Name: "cmpequal",
-	Doc: "Check arg types of cmp.Equal",
-	Requires:
-[]*analysis.Analyzer{inspect.Analyzer},
-	Run: run,
+var Analyzer = &analysis.Analyzer{
+	Name:     "cmpequal",
+	Doc:      "Check arg types of cmp.Equal",
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+	Run:      run,
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
@@ -26,20 +28,47 @@ func run(pass *analysis.Pass) (interface{}, error) {
 			return // not a function call
 		}
 
-		if fn.FullName() != "github.com/google/go-cmp/cmp.Equal" {     // should also check cmp.Diff, etc.
+		if fn.FullName() != "github.com/google/go-cmp/cmp.Equal" { // should also check cmp.Diff, etc.
 			return // not a call to Equal
 		}
 
 		typ0 := pass.TypesInfo.Types[call.Args[0]].Type
 		typ1 := pass.TypesInfo.Types[call.Args[1]].Type
 		if !types.Identical(typ0, typ1) {
-			pass.Reportf(call.Pos(),
-				"cmp.Equal's arguments must have the same type; "+
-						"is called with %v and %v",
-				typ0, typ1)
-		}	}
+			var fixes []analysis.SuggestedFix
+			if isPointerTo(typ0, typ1) {
+				fixes = append(fixes, fixDereference(pass, call.Args[0]))
+			} else if isPointerTo(typ1, typ0) {
+				fixes = append(fixes, fixDereference(pass, call.Args[1]))
+			}
+			pass.Report(analysis.Diagnostic{
+				Pos: call.Pos(), End: call.End(),
+				Message: fmt.Sprintf("cmp.Equal's arguments must have the same type; "+
+					"is called with %v and %v", typ0, typ1),
+				SuggestedFixes: fixes,
+			})
+		}
+	}
 	inspect.Preorder(
 		[]ast.Node{(*ast.CallExpr)(nil)},
 		inspectNode)
 	return nil, nil
+}
+
+func isPointerTo(a, b types.Type) bool {
+	if ptr, ok := a.(*types.Pointer); ok {
+		return types.Identical(ptr.Underlying(), b)
+	}
+	return false
+}
+
+func fixDereference(pass *analysis.Pass, expr ast.Expr) analysis.SuggestedFix {
+	// dereference typ0
+	var buf bytes.Buffer
+	ast.Fprint(&buf, pass.Fset, ast.StarExpr{X: expr}, nil)
+	fix := analysis.SuggestedFix{
+		Message:   "derefenence pointer",
+		TextEdits: []analysis.TextEdit{{expr.Pos(), expr.End(), buf.Bytes()}},
+	}
+	return fix
 }
